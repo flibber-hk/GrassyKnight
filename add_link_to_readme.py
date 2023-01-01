9d5883c1b2fa932d84cc8881c96ad344c394f67e
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2023-01-01 16:49:53 +0000

    Update add_link_to_readme.py

--- add_link_to_readme.py
+++ add_link_to_readme.py
@@ -2,13 +2,19 @@
 
 import sys
 
+lines = []
+
 # This could be done with sed, but there's some relevant differences between
 # mac's sed and linux's sed and it just doesn't feel worth it.
-with open(sys.argv[1]) as fin, open(sys.argv[2]) as fout:
+with open(sys.argv[1]) as fin:
     for index, line in enumerate(fin):
         if index == 2:
-            fout.write(
+            lines.append(
                 "For a nicely formatted version of this README, go to "
                 "https://github.com/itsjohncs/GrassyKnight#grassy-knight\n\n")
 
+        lines.append(line)
+
+with open(sys.argv[2]) as fout:
+    for line in lines:
         fout.write(line)
